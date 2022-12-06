5a68415fd6a84ccff1d55305df07e019d8e2d1fc
Lee Joon <jun29334@naver.com>
2022-12-07 07:47:51 +0900

    [ADD] PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 2022.12.05 / LJ
+/// 플레이어 조작 관련
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
