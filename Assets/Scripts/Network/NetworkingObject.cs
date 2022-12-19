386950a58562aac8d3c19c7b9c00a03205d2e884
정은성 <dmstjd0911@naver.com>
2022-12-19 09:10:53 +0900

    ADD Network Setting

---
+++ Assets/Scripts/Network/NetworkingObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkingObject : MonoBehaviour
+{
+    public int Id { get; set; }
+    public bool isMine { get; set; }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
