679f955bd4e28364f8b32f58eda070c8f277bb43
hsh0517 <pelino0517@gmail.com>
2022-12-21 17:53:01 +0900

    Update Log

--- Assets/Sehyeon/AvoidLog/Scripts/Log.cs
+++ Assets/Sehyeon/AvoidLog/Scripts/Log.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Log : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool IsStarted;
+    public int RotY;
+
+    private void Update()
     {
-        
+        if (!IsStarted) return;
+        transform.eulerAngles = new Vector3(0, RotY, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.gameObject.TryGetComponent(out PlayerController pc))
+            {
+                // 넉백 처리
+                
+            }
+        }
     }
 }
