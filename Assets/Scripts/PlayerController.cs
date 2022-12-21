4b513102fcd1e62d05e29293031d67b7908ca1a8
LeeJoon <leejun9334@gmail.com>
2022-12-21 20:32:38 +0900

    [#update] 플레이어 중력 구현 및 플레이어 점프 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,11 @@ public class PlayerController : MonoBehaviour
     [Header("Player Movement Stat")]
     [Range(0f, 100f)] [SerializeField] private float speed;
     CharacterController controller;
+    [SerializeField] private bool jump;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] [Range(0f, 10f)] private float jumpHeight;
+    [SerializeField] [Range(0f, 10f)] private float jumpTimeout;
+    private float jumpTimer;
 
     private float movementX;
     private float movementY;
@@ -24,7 +29,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float targetRotation = 0f;
     [SerializeField] private float rotationVelocity;
     private float rotationTime = 0.12f;
-    private float verticalVelocity;
+    
+
+    [Header("Player Gravity")]
+    [SerializeField] [Range(-20f, 20f)] private float gravity = -9.81f;
+    [SerializeField] private float verticalVelocity;
 
 
     private void Awake()
@@ -42,7 +51,9 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) onJump();
+        if (IsCheckGrounded()) jump = false;
+
     }
 
     private void FixedUpdate()
@@ -69,6 +80,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY).normalized;
 
+        Vector3 targetDirection = Vector3.zero;
 
         if (movement != Vector3.zero)
         {
@@ -78,14 +90,13 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
         }
 
-        if (!(movementX == 0 && movementY == 0))
+        if ((!(movementX == 0 && movementY == 0)))
         {
             // StateManager
             playerStateManager.State = PlayerState.Move;
 
-            Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
+            targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
             //rb.velocity = movement * speed * Time.deltaTime;
-            controller.Move(targetDirection.normalized * (speed * Time.deltaTime) + new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
         }
         else
         {
@@ -93,5 +104,65 @@ public class PlayerController : MonoBehaviour
             if (playerStateManager.State != PlayerState.AFK)
                 playerStateManager.State = PlayerState.Idle;
         }
+
+        if (jump) // 중력
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+
+        controller.Move(targetDirection.normalized * (speed * Time.deltaTime) + new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// 2022.12.21 / LJ
+    /// 바닥 검사
+    /// </summary>
+    private bool IsCheckGrounded()
+    {
+        if (controller.isGrounded)
+        {
+            return true;
+        }
+        var ray = new Ray(this.transform.position + Vector3.up * 0.1f, Vector3.down);
+        float maxDistance = 0.3f;
+        Debug.DrawRay(transform.position + Vector3.up * 0.1f, Vector3.down * maxDistance, Color.yellow);
+        return Physics.Raycast(ray, maxDistance, ground);
+    }
+
+    /// <summary>
+    /// 2022.12.21 / LJ
+    /// Space키를 눌렀을 때 실행
+    /// </summary>
+    void onJump()
+    {
+        if (IsCheckGrounded() && !jump) // 점프가 가능 하다면
+        {
+            verticalVelocity = 0f;
+            JumpAndGravity();
+        }
+        else return;
+    }
+
+    /// <summary>
+    /// 2022.12.21 / LJ
+    /// 점프 및 중력 관리
+    /// </summary>
+    void JumpAndGravity()
+    {
+        verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        jumpTimer = 0f;
+        JumpTimerOut();
+    }
+
+    void JumpTimerOut()
+    {
+        jumpTimer += Time.deltaTime;
+        if (jumpTimer < jumpTimeout)
+        {
+            Invoke("JumpTimerOut", Time.deltaTime);
+            return;
+        }
+        jump = true;
+        return;
     }
 }
