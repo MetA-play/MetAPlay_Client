a184f24dd9db1352383d96e87a933389b24c585a
LeeJoon <leejun9334@gmail.com>
2022-12-28 18:26:03 +0900

    [#update] 플레이어 커스터마이징 각 부위 파츠 적용 및 망토 색상 변경

--- Assets/Scripts/Define.cs
+++ Assets/Scripts/Define.cs
@@ -7,3 +7,34 @@ namespace Define
         AFK
     }
 }
+
+namespace Customization
+{
+    public enum Part
+    {
+        Head,
+        Body,
+        Leg
+    }
+
+    public enum Head
+    {
+        선택안함,
+        요리사모자,
+        신사모자,
+        인디언머리띠,
+        칼
+    }
+
+    public enum Body
+    {
+        선택안함,
+        망토
+    }
+
+    public enum Leg
+    {
+        선택안함,
+        오리발
+    }
+}
