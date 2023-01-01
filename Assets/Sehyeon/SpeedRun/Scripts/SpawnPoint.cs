c468fe63a3c725998f2beb5f779a8342136dd2f4
hsh0517 <pelino0517@gmail.com>
2023-01-02 01:27:24 +0900

    Update spawnpoint

--- Assets/Sehyeon/SpeedRun/Scripts/SpawnPoint.cs
+++ Assets/Sehyeon/SpeedRun/Scripts/SpawnPoint.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.Protocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,7 +11,17 @@ public class SpawnPoint : MonoBehaviour
         {
             if (other.TryGetComponent(out NetworkingObject obj))
             {
-                
+                if (obj.isMine)
+                {
+                    TransformInfo spawnPoint = new TransformInfo
+                    {
+                        Pos = new Vector() { X = transform.position.x, Y = transform.position.y, Z = transform.position.z }
+                    };
+
+                    C_SetSpawnPoint setSpawnPointPacket = new C_SetSpawnPoint();
+                    setSpawnPointPacket.SpawnPoint = spawnPoint;
+                    NetworkManager.Instance.Send(setSpawnPointPacket);
+                }
             }
         }
     }
