5b45a4f8b7adcffc7393a0c5dea254f3cb144ed5
hsh0517 <pelino0517@gmail.com>
2023-09-04 13:34:49 +0900

    주소 변경

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -44,7 +44,7 @@ public class NetworkManager : MonoBehaviour
 
         string host = Dns.GetHostName();
         IPHostEntry ipHost = Dns.GetHostEntry(host);
-        IPAddress ipAddr = IPAddress.Parse("192.168.214.234");
+        IPAddress ipAddr = IPAddress.Parse("192.168.42.235");
         IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
         Connector connector = new Connector();
